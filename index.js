8d362fec983c730bd3dddfb61f21ba0e260c2d08
devinhaeng <gjgns51@naver.com>
2019-09-07 13:11:08 +0900

    Data Type on JS

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 // console.log('I am working. I am JS. I am Beautiful. I am worth it');
 
-let a = 221;
-var b = a - 5;
-a = 4;
-console.log(b, a);
+// let a = 221;
+// var b = a - 5;
+// a = 4;
+// console.log(b, a);
 
 // let => 변수를 수정할수 있다.
 // const => 변수를 수정할수 없다.
 // var => 일반적으로 쓰는 변수 변수를 수정할수 있다.
+
+/*
+to do : finish this
+to do : finish this
+to do : finish this         -> 멀티 주석
+to do : finish this
+to do : finish this
+*/
+
+/*
+String
+const what = "Heoinhaeng";
+
+console.log(what);
+*/
+
+/*
+Boolean
+const wat = true; or false;
+*/
+
+/*
+Number
+const wat = 666;
+*/
+
+/*
+Float 
+const wat = 55.1;
+*/
