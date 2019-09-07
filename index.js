4186745282c5a2deccc341a294061ebf48e6e705
devinhaeng <gjgns51@naver.com>
2019-09-07 12:56:33 +0900

    let, const, var

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // console.log('I am working. I am JS. I am Beautiful. I am worth it');
 
 let a = 221;
-let b = a - 5;
+var b = a - 5;
 a = 4;
 console.log(b, a);
+
+// let => 변수를 수정할수 있다.
+// const => 변수를 수정할수 없다.
+// var => 일반적으로 쓰는 변수 변수를 수정할수 있다.
