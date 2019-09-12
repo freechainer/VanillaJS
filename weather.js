f04a9124f48b401459398e45238e683a72338e58
devinhaeng <gjgns51@naver.com>
2019-09-12 14:57:07 +0900

    Getting the Weather part One (Geolocation)

---
+++ weather.js
@@ -0,0 +1,39 @@
+const API_KEY = "9327724958d184925452c1b126bb9c7b";
+const COORDS = 'coords';
+
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+    console.log("Cant access geo location");
+}
+
+function askForCoords() {
+    navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords() {
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null) {
+        askForCoords();
+    } else {
+        // getWeather
+    }
+}
+
+function init() {
+    loadCoords();
+}
+
+init();
