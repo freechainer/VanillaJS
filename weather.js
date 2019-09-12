997fc48e0176a8efb376a664f37e3f29aa8ffd66
devinhaeng <gjgns51@naver.com>
2019-09-12 16:29:01 +0900

    Getting the Weather part Two (API)

--- weather.js
+++ weather.js
@@ -1,5 +1,20 @@
+const weather = document.querySelector(".js-weather");
+
 const API_KEY = "9327724958d184925452c1b126bb9c7b";
 const COORDS = 'coords';
+
+function getWeather(lat, lng) {
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    ).then(function(response) {
+        return response.json();
+    })
+    .then(function(json) {
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 
 function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -13,6 +28,7 @@ function handleGeoSucces(position) {
         longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude)
 }
 
 function handleGeoError() {
@@ -28,7 +44,8 @@ function loadCoords() {
     if(loadedCoords === null) {
         askForCoords();
     } else {
-        // getWeather
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
